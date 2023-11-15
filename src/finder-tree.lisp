391ba04b9b07b68bb0f15f696c72ad59deb85705
Alan <1418294+alangalvino@users.noreply.github.com>
2023-11-15 20:49:07 -0300

    Finder implementation using AVL binary tree

---
+++ src/finder-tree.lisp
@@ -0,0 +1,26 @@
+(in-package :consistent-hashing)
+
+(defclass finder-tree (finder) ()
+  (:documentation "A finder implemented using a avl binary tree."))
+
+(defun make-finder-tree ()
+  (make-finder :data-structure
+               (trees:make-binary-tree :avl #'(lambda (x y)
+                                                (< x y))
+                                       :key #'virtual-node-key)))
+
+(defmethod finder-insert ((finder finder) (virtual-node virtual-node))
+  (let ((tree (finder-data-structure finder)))
+    (trees:insert virtual-node tree)))
+
+(defmethod finder-delete ((finder finder) (virtual-node virtual-node))
+  (let ((tree (finder-data-structure finder)))
+    (trees:delete (virtual-node-key virtual-node) tree)))
+
+(defmethod finder-greater-or-equal ((finder finder) value)
+  (let ((tree (finder-data-structure finder)))
+    (trees:upper-bound value tree)))
+
+(defmethod finder-minimum ((finder finder))
+  (let ((tree (finder-data-structure finder)))
+    (trees:minimum tree)))
